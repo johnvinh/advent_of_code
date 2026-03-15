74f3f76cf6c08880c05fbf98b1d0f600bb085630
John Vinh <john.michael.vinh2@gmail.com>
2026-03-14 20:09:50 -0400

    Refactor to return result

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use std::fs;
 use regex::Regex;
 
-fn get_password(rotations: String) -> i32 {
+fn get_password(rotations: &str) -> Result<i32, &str> {
     let mut current_position = 50;
     let re = Regex::new(r"^([A-Za-z])(.+)$").unwrap();
     let mut num_times_at_zero = 0;
     
     for line in rotations.lines() {
         let Some(caps) = re.captures(line) else {
-            println!("Bad");
-            return 0;
+            return Err("Something went wrong with the input!");
         };
         
         let num = &caps[2].parse::<i32>().unwrap();
         let delta = match &caps[1] {
             "L" => -*num,
             "R" => *num,
-            _ => return -1,
+            _ => return Err("Something went wrong with the input!"),
         };
         let max = 99;
         current_position = ((current_position + delta) % (max + 1) + (max + 1)) % (max + 1);
@@ -24,11 +23,14 @@ fn get_password(rotations: String) -> i32 {
             num_times_at_zero += 1;
         }
     }
-    num_times_at_zero
+    Ok(num_times_at_zero)
 }
 
 fn main() -> Result<(), std::io::Error> {
     let file_contents = fs::read_to_string("input.txt")?;
-    println!("{}", get_password(file_contents));
+    // println!("{}", get_password(file_contents));
+    if let Ok(result) = get_password(&file_contents) {
+        println!("The password is: {}", result)
+    }
     Ok(())
 }
